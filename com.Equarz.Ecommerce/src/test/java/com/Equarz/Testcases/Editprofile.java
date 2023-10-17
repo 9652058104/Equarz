9fa20a798ba85f8b992a35aa8dce03f8e5e2ef1f
DELL <DELL@DESKTOP-BCFLHSK>
2023-10-17 10:13:51 +0530

    Editprofile

--- com.Equarz.Ecommerce/src/test/java/com/Equarz/Testcases/Editprofile.java
+++ com.Equarz.Ecommerce/src/test/java/com/Equarz/Testcases/Editprofile.java
@@ -1,5 +1,50 @@
 package com.Equarz.Testcases;
 
-public class Editprofile {
+	
 
-}
+	import java.awt.AWTException;
+
+	import org.testng.annotations.BeforeClass;
+	import org.testng.annotations.BeforeMethod;
+	import org.testng.annotations.Test;
+
+	import com.Pageobjects.*;
+	import com.Pageobjects.Login_Functionality;
+	import com.base.Testbase;
+
+	public class Editprofile extends Testbase {
+		Editprofile ef;
+		Login_Functionality lg;
+		
+		
+		public Editprofile ()
+		{
+			super();
+		}
+		@BeforeClass
+		public void login()
+		{
+			Setup();
+			lg=new Login_Functionality(driver);
+			lg.Verifylogin();
+			
+		}
+		@BeforeMethod
+		public void initialize() 
+		{
+			
+//			Setup();
+//			lg=new Login_Functionality(driver);
+//			lg.validateLogin();
+			ef=new Editprofile (driver);		
+					
+		}
+		@Test()
+		public void search() throws InterruptedException, AWTException
+		{
+			ef.Editprofie();
+			
+		}
+
+	}
+
