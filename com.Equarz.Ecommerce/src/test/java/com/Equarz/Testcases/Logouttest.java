614c2fb5a36adb2c7e4e2178528bcd27fdc0fb65
DELL <DELL@DESKTOP-BCFLHSK>
2023-10-17 20:00:36 +0530

    Addressfunctionaliy

--- com.Equarz.Ecommerce/src/test/java/com/Equarz/Testcases/Logouttest.java
+++ com.Equarz.Ecommerce/src/test/java/com/Equarz/Testcases/Logouttest.java
@@ -1,5 +1,61 @@
 package com.Equarz.Testcases;
 
-public class Logouttest {
+	
 
-}
+	import java.awt.AWTException;
+
+	import org.testng.annotations.BeforeClass;
+	import org.testng.annotations.BeforeMethod;
+	import org.testng.annotations.Test;
+
+
+	import com.Pageobjects.*;
+	import com.Pageobjects.Login_Functionality;
+	import com.base.Testbase;
+
+	public class Logouttest extends Testbase {
+		private void Logoutfuncti() {
+			// TODO Auto-generated method stub
+
+		} lf;
+		Login_Functionality lg;
+		
+		
+		public Logouttest ()
+		{
+			super();
+		}
+		
+		@BeforeMethod
+		public void initialize() 
+		{
+			
+			Setup();
+			lg=new Login_Functionality(driver);
+			lg.Verifylogin();
+			lf=new Logoutfunctionality (driver);		
+					
+		}
+		@Test()
+		public void logout() throws InterruptedException, AWTException
+		{
+			lf.logout();
+			
+		}
+		@Test()
+		public void refresh() throws InterruptedException, AWTException
+		{
+			lf.refresh();
+			
+		}
+		@Test()
+		public void back() throws InterruptedException, AWTException
+		{
+			lf.back();
+			
+		}
+
+
+
+	}
+
